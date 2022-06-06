b2fe50fe52f0be84395073b5cf0d9f910d2c06f1
De-Bill <podshivalov.o29@gmail.com>
2022-06-06 22:25:40 +0300

    feat: add exam documents parser

---
+++ DosadoExamsBot/WordTools/DocumentParser.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Aspose.Words;
+
+namespace DosadoExamsBot.WordTools;
+
+public static class DocumentParser
+{
+    private static readonly string MainDirectory;
+    private static readonly string FilesDirectory;
+
+    static DocumentParser()
+    {
+        MainDirectory = GetCodeBaseDirectory(Assembly.GetExecutingAssembly());
+        FilesDirectory = MainDirectory + @"DosadoExamsBot\DosadoExamsBot" + @"\Files\";
+    }
+    
+    public static List<string> GetQuestions(string examFileNameWithExtension)
+    {
+        var questions = new List<string>();
+        var doc = new Document(FilesDirectory + examFileNameWithExtension);
+
+        var sec = (Section)doc.GetChild(NodeType.Section, 0, true);
+        foreach (var paragraph in sec.Body.Paragraphs.Select(p => (Paragraph)p).Where(p => p.IsListItem))
+        {
+            questions.Add(paragraph.GetText()[..^1]);
+        }
+
+        return questions;
+    }
+    
+    private static string GetCodeBaseDirectory(Assembly assembly)
+    {
+        var uri = new Uri(assembly.Location);
+        string mainFolder = Path.GetDirectoryName(uri.LocalPath)
+            !.Substring(0, uri.LocalPath.IndexOf("DosadoExamsBot", StringComparison.Ordinal));
+        return mainFolder;
+    }
+}
