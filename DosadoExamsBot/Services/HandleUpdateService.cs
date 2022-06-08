156739bb7d231df7b9428b0caa18ce23cc00a970
De-Bill <podshivalov.o29@gmail.com>
2022-06-08 22:40:20 +0300

    feat: clean up business logic xD

--- DosadoExamsBot/Services/HandleUpdateService.cs
+++ DosadoExamsBot/Services/HandleUpdateService.cs
@@ -3,7 +3,6 @@ using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace DosadoExamsBot.Services;
@@ -12,7 +11,8 @@ public class HandleUpdateService
 {
     private readonly ITelegramBotClient _botClient;
     private readonly ILogger<HandleUpdateService> _logger;
-    private readonly Exam _mathematicalStatisticsExam = new("MathematicalStatisticsExam.docx");
+    private Exam _exam;
+    private static int _streakCount = 0;
 
     public HandleUpdateService(ITelegramBotClient botClient, ILogger<HandleUpdateService> logger)
     {
@@ -26,8 +26,6 @@ public class HandleUpdateService
         {
             UpdateType.Message            => BotOnMessageReceived(update.Message!),
             UpdateType.CallbackQuery      => BotOnCallbackQueryReceived(update.CallbackQuery!),
-            UpdateType.InlineQuery        => BotOnInlineQueryReceived(update.InlineQuery!),
-            UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(update.ChosenInlineResult!),
             _                             => UnknownUpdateHandlerAsync(update)
         };
 
@@ -49,94 +47,50 @@ public class HandleUpdateService
 
         var action = message.Text!.Split(' ')[0] switch
         {
-            "/inline"   => SendInlineKeyboard(_botClient, message),
-            "/keyboard" => SendReplyKeyboard(_botClient, message),
-            "/remove"   => RemoveKeyboard(_botClient, message),
-            "/МатСтат"  => RequestMathStatQuestion(_botClient, message),
+            "/MathStat"  => RequestMathStatQuestion(_botClient, message),
+            "/DataBases"  => RequestDataBasesQuestion(_botClient, message),
             _           => Usage(_botClient, message)
         };
         Message sentMessage = await action;
         _logger.LogInformation("The message was sent with id: {sentMessageId}", sentMessage.MessageId);
-
-        // Send inline keyboard
-        // You can process responses in BotOnCallbackQueryReceived handler
-        static async Task<Message> SendInlineKeyboard(ITelegramBotClient bot, Message message)
+        
+        static async Task<Message> RequestMathStatQuestion(ITelegramBotClient bot, Message message)
         {
             await bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
-
-            // Simulate longer running task
-            await Task.Delay(500);
 
             InlineKeyboardMarkup inlineKeyboard = new(
                 new[]
                 {
-                    // first row
-                    new []
-                    {
-                        InlineKeyboardButton.WithCallbackData("1.1", "11"),
-                        InlineKeyboardButton.WithCallbackData("1.2", "12"),
-                    },
-                    // second row
-                    new []
-                    {
-                        InlineKeyboardButton.WithCallbackData("2.1", "21"),
-                        InlineKeyboardButton.WithCallbackData("2.2", "22"),
-                    },
+                    InlineKeyboardButton.WithCallbackData("Следующий вопрос 🈯", "MathStat"),
+                    InlineKeyboardButton.WithCallbackData("Закончить лютую ботку 🚬", "stop"),
                 });
 
             return await bot.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                  text: "Choose",
+                                                  text: "Okay let's go",
                                                   replyMarkup: inlineKeyboard);
         }
-
-        static async Task<Message> SendReplyKeyboard(ITelegramBotClient bot, Message message)
+        
+        static async Task<Message> RequestDataBasesQuestion(ITelegramBotClient bot, Message message)
         {
-            ReplyKeyboardMarkup replyKeyboardMarkup = new(
+            await bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+            InlineKeyboardMarkup inlineKeyboard = new(
                 new[]
                 {
-                        new KeyboardButton[] { "1.1", "1.2" },
-                        new KeyboardButton[] { "2.1", "2.2" },
-                })
-                {
-                    ResizeKeyboard = true
-                };
+                    InlineKeyboardButton.WithCallbackData("Следующий вопрос 🈯", "MathStat"),
+                    InlineKeyboardButton.WithCallbackData("Закончить лютую ботку 🚬", "stop"),
+                });
 
             return await bot.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                  text: "Choose",
-                                                  replyMarkup: replyKeyboardMarkup);
-        }
-
-        static async Task<Message> RemoveKeyboard(ITelegramBotClient bot, Message message)
-        {
-            return await bot.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                  text: "Removing keyboard",
-                                                  replyMarkup: new ReplyKeyboardRemove());
-        }
-
-        static async Task<Message> RequestMathStatQuestion(ITelegramBotClient bot, Message message)
-        {
-            ReplyKeyboardMarkup replyKeyboardMarkup = new(
-                new[]
-                {
-                    new KeyboardButton("Следующий вопрос 🈯"),
-                    new KeyboardButton("Закончить лютую ботку 🚬")
-                })
-            {
-                ResizeKeyboard = true
-            };
-
-            return await bot.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                  text: "Следующий вопрос?",
-                                                  replyMarkup: replyKeyboardMarkup);
+                                                  text: "Okay let's go",
+                                                  replyMarkup: inlineKeyboard);
         }
 
         static async Task<Message> Usage(ITelegramBotClient bot, Message message)
         {
             const string usage = "Usage:\n" +
-                                 "/inline   - send inline keyboard\n" +
-                                 "/keyboard - send custom keyboard\n" +
-                                 "/remove   - remove custom keyboard\n" +
-                                 "/МатСтат  - случайный вопрос из экзамена по МатСтату";
+                                 "/DataBases - случайный вопрос из экзамаена по БД\n" +
+                                 "/MathStat  - случайный вопрос из экзамена по МатСтату";
 
             return await bot.SendTextMessageAsync(chatId: message.Chat.Id,
                                                   text: usage,
@@ -147,46 +101,44 @@ public class HandleUpdateService
     // Process Inline Keyboard callback data
     private async Task BotOnCallbackQueryReceived(CallbackQuery callbackQuery)
     {
+        if (callbackQuery.Data == "MathStat")
+        {
+            if (_exam.ExamName is not ExamName.MathematicalStatistics)
+            {
+                _exam = new Exam(ExamName.MathematicalStatistics, "MathematicalStatisticsExam.docx");
+                _streakCount = 0;
+            }
+
+            _streakCount++;
+            await _botClient.EditMessageTextAsync(callbackQuery.Message.Chat.Id,
+                                                    callbackQuery.Message.MessageId,
+                                                    _exam.GetRandomQuestion()
+                                                    + $" (Current streak: {_streakCount})",
+                                                    replyMarkup: callbackQuery.Message.ReplyMarkup);
+        }
         
-        await _botClient.AnswerCallbackQueryAsync(
-            callbackQueryId: callbackQuery.Id,
-            text: $"Received {callbackQuery.Data}");
-
-        await _botClient.SendTextMessageAsync(
-            chatId: callbackQuery.Message!.Chat.Id,
-            text: $"Received {callbackQuery.Data}");
-    }
-
-    #region Inline Mode
-
-    private async Task BotOnInlineQueryReceived(InlineQuery inlineQuery)
-    {
-        _logger.LogInformation("Received inline query from: {inlineQueryFromId}", inlineQuery.From.Id);
+        if (callbackQuery.Data == "DataBases")
+        {
+            if (_exam.ExamName is not ExamName.DataBases)
+            {
+                _exam = new Exam(ExamName.DataBases, "DataBasesExam.docx");
+                _streakCount = 0;
+            }
+            
+            _streakCount++;
+            await _botClient.EditMessageTextAsync(callbackQuery.Message.Chat.Id, 
+                                                    callbackQuery.Message.MessageId, 
+                                                    _exam.GetRandomQuestion()
+                                                    + $" (Current streak: {_streakCount})",
+                                                    replyMarkup: callbackQuery.Message.ReplyMarkup);
+        }
         
-        InlineQueryResult[] results = {
-            // displayed result
-            new InlineQueryResultArticle(
-                id: "3",
-                title: "TgBots",
-                inputMessageContent: new InputTextMessageContent(
-                    "hello"
-                )
-            )
-        };
-
-        await _botClient.AnswerInlineQueryAsync(inlineQueryId: inlineQuery.Id,
-                                                results: results,
-                                                isPersonal: true,
-                                                cacheTime: 0);
+        if (callbackQuery.Data == "stop")
+        {
+            await BotOnMessageReceived(new Message());
+            _streakCount = 0;
+        }
     }
-
-    private Task BotOnChosenInlineResultReceived(ChosenInlineResult chosenInlineResult)
-    {
-        _logger.LogInformation("Received inline result: {chosenInlineResultId}", chosenInlineResult.ResultId);
-        return Task.CompletedTask;
-    }
-
-    #endregion
 
     private Task UnknownUpdateHandlerAsync(Update update)
     {
